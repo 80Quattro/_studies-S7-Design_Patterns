ac6700e65b03420ad10d5954b2891159daf28bb7
80Quattro <muzyczek2@gmail.com>
2022-11-03 16:40:04 +0100

    Factory

--- lab01/src/main/java/pl/edu/wszib/lab01/builder/record/Application.java
+++ lab01/src/main/java/pl/edu/wszib/lab01/builder/record/Application.java
@@ -3,7 +3,11 @@ package pl.edu.wszib.lab01.builder.record;
 public class Application {
 
     public static void main(String[] args) {
-        Person.male("Rafal", "Jania", 24, new Address("", "", ""));
-        Person.female();
+        Person male1 = Person.male("Rafal", "Jania", 24, new Address("", "", ""));
+        Person felame1 = Person.female("Rafalina", "Jania", 26, new Address("", "", ""));
+
+        final PersonFactory personFactory = new PersonFactory();
+        personFactory.male2("Rafal", "Jania", 24, new Address("", "", ""));
+        personFactory.female2("Rafalina", "Jania", 26, new Address("", "", ""));
     }
 }
