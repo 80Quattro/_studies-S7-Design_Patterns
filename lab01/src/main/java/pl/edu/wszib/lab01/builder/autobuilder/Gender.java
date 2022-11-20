7263914a9fdac06b75cbb9c27580e15552bd37fc
80Quattro <muzyczek2@gmail.com>
2022-11-20 08:48:38 +0100

    Change project structure - add lab02

--- lab01/src/main/java/pl/edu/wszib/lab01/builder/autobuilder/Gender.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.wszib.lab01.builder.autobuilder;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-}
--- lab01/src/main/java/pl/edu/wszib/lab01/singleton/Application.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.wszib.lab01.singleton;
-
-public class Application {
-
-    public static void main(String[] args) {
-
-    }
-
-}
